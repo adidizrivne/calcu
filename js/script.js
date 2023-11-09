660491a1a5545980055bfe6b0f36db334ede36b5
clinhfist <49395348+adidizrivne@users.noreply.github.com>
2023-11-09 13:42:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// script.js
+
+document.getElementById("calculate").addEventListener("click", function () {
+  const hours = parseFloat(document.getElementById("hours").value);
+  const rank = parseFloat(document.getElementById("rank").value);
+  const discipline = document.getElementById("discipline").checked ? 0.1 : 1;
+  const productivity = parseFloat(document.getElementById("productivity").value);
+  const nightHours = parseFloat(document.getElementById("nightHours").value);
+
+  const hourlyRate = 68.00;
+
+  // Розрахунок для денних годин
+  const sum1 = hours * hourlyRate;
+  const sum2 = sum1 * rank;
+  const sum3 = sum1 * discipline;
+  const sum4 = sum1 * productivity;
+
+  const totalDayHours = sum1 + sum2 + sum3 + sum4;
+
+  // Розрахунок для нічних годин
+  const nightRate = hourlyRate * 1.2;
+  const sum5 = nightHours * nightRate;
+  const sum6 = sum5 * rank;
+  const sum7 = sum5 * discipline;
+  const sum8 = sum5 * productivity;
+
+  const totalNightHours = sum5 + sum6 + sum7 + sum8;
+
+  const totalPayment = (totalDayHours + totalNightHours).toFixed(2);
+
+  document.getElementById("result").textContent = totalPayment + " грн";
+});
